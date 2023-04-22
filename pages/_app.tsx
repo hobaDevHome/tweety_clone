6d782d26b3dfef8db8df3217275e2a8755ec5912
hoba-android <heba@amy.app>
2023-04-22 04:17:17 +0200

    clone uploade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from '../components/Layout';
+import Modal from '../components/Modal';
+import LoginModal from '../components/modals/LoginModal';
+import RegisterModal from '../components/modals/RegisterModal';
+import EditModal from '../components/modals/EditModal';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <LoginModal />
+      <RegisterModal />
+      <EditModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
